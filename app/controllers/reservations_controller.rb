d3222a6255a7f8683a949b0559da7cc4850fab13
Juliette Hutton <juliettehutton2@gmail.com>
2024-05-28 16:17:03 -0400

    created controller and actions

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,40 @@
 class ReservationsController < ApplicationController
+  before_action :set_kitchen, only: [:new, :create]
+  before_action :set_reservation, only: [:show, :update]
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.kitchen = @kitchen
+    if @reservation.save
+      redirect_to kitchen_path(@kitchen)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def update
+    @reservation.update(reservation_params)
+    redirect_to reservation_path(@reservation)
+  end
+
+  private
+
+  def set_kitchen
+    @kitchen = Kitchen.find(params[:kitchen_id])
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :total_price, :status)
+  end
 end

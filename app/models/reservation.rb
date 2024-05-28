c882a8dfc7bb18e2b563f39fb91fce4bdf4fdcda
Juliette Hutton <juliettehutton2@gmail.com>
2024-05-28 15:00:26 -0400

    deleted relations for the meantime

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,4 @@
 class Reservation < ApplicationRecord
-  belongs_to :user
-  belongs_to :kitchen
+  # belongs_to :user
+  # belongs_to :kitchen
 end
--- db/migrate/20240528185316_create_reservations.rb
+++ db/migrate/20240528185316_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[7.1]
   def change
     create_table :reservations do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :kitchen, null: false, foreign_key: true
+      # t.references :user, null: false, foreign_key: true
+      # t.references :kitchen, null: false, foreign_key: true
       t.date :start_date
       t.date :end_date
       t.float :total_price

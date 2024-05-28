553e6c092e052df990c77ae3d6fd2766375473d2
humayun khan <humayun.n.k@gmail.com>
2024-05-28 14:04:31 -0400

    updated user model and made show page accessible without auth

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :show]
 
   def home
   end
+
 end

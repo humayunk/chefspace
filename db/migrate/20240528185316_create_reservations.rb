9fbe267b91d732be484b5acf73d84e195bd7226e
Juliette Hutton <juliettehutton2@gmail.com>
2024-05-28 14:54:29 -0400

    created reservation model

---
+++ db/migrate/20240528185316_create_reservations.rb
@@ -0,0 +1,14 @@
+class CreateReservations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reservations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :kitchen, null: false, foreign_key: true
+      t.date :start_date
+      t.date :end_date
+      t.float :total_price
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end

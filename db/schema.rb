2b20d5bd9433ee2ecf412ab59746c3fd89d25299
Vaeda Solomon <vaeda.solomon@gmail.com>
2024-05-28 15:27:01 -0400

    kitchen model updated, fixed typo

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_185316) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_190841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "kitchens", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "address"
+    t.integer "daily_rate"
+    t.string "kitchen_type"
+    t.text "description"
+    t.boolean "has_parking"
+    t.boolean "has_fire_safety"
+    t.boolean "has_refrigeration"
+    t.string "storage_type"
+    t.boolean "has_oven"
+    t.boolean "has_stove"
+    t.boolean "has_fryer"
+    t.boolean "has_dishwasher"
+    t.boolean "has_cookware"
+    t.boolean "has_cutlery"
+    t.boolean "availability"
+    t.integer "prep_tables"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_kitchens_on_user_id"
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.date "start_date"
@@ -40,4 +63,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_185316) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "kitchens", "users"
 end

d68cdfa5e58595202617a25b10207d20a7258acd
jeff prince <jeffryaime@gmail.com>
2024-05-30 18:32:54 -0400

    signed up front end

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,3 @@
-# Pin npm packages by running ./bin/importmap
-
-# FROM LECTURE
-
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true

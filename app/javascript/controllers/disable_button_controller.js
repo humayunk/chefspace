5c90167f3cebfdd4c7722bfda7bba4a86c1adcd9
Vaeda Solomon <vaeda.solomon@gmail.com>
2024-05-31 14:11:59 -0400

    not working yet2

--- app/javascript/controllers/disable_button_controller.js
+++ app/javascript/controllers/disable_button_controller.js
@@ -4,9 +4,20 @@
 //   static targets = ["button"]
 
 //   disableButton() {
+//     this.element.innerText = "Bingo!";
+//     this.element.setAttribute("disabled", "");
+//   }
+// }
+
+
+// import { Controller } from "stimulus";
+
+// export default class extends Controller {
+//   static targets = ["button"]
+
+//   disableButton() {
 //     this.buttonTargets.forEach(button => {
 //       button.disabled = true;
-//       button.innerText = "Processing...";
 //     });
 //   }
 // }
